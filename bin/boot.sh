321b17d98760bf647bb371a20ef890878cc53f6c
Shchori <nir.shc@gmail.com>
2017-09-12 13:26:19 +0300

    Update boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -4,15 +4,15 @@ export LD_LIBRARY_PATH=$APP_ROOT/openresty/luajit/lib:$LD_LIBRARY_PATH
 
 export PATH=$APP_ROOT/openresty/bin:$PATH
 
-mkdir -p /tmp/staged/app/openresty/nginx/logs
-touch /tmp/staged/app/openresty/nginx/logs/error.log
-mkdir -p /tmp/staged/app/openresty/nginx/client_body_temp
+mkdir -p /tmp/app/openresty/nginx/logs
+touch /tmp/app/openresty/nginx/logs/error.log
+mkdir -p /tmp/app/openresty/nginx/client_body_temp
 
 mkdir -p $APP_ROOT/nginx/logs
 touch $APP_ROOT/nginx/logs/error.log
 touch $APP_ROOT/nginx/logs/access.log
 
-cp -a $APP_ROOT/openresty/lualib /tmp/staged/app/openresty/lualib/
+cp -a $APP_ROOT/openresty/lualib /tmp/app/openresty/lualib/
 
 conf_file=$APP_ROOT/openresty/nginx/conf/nginx.conf
 if [ -f $APP_ROOT/scripts/nginx.conf ]
